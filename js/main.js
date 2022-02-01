feb399c88a74e7457dd63687d83091dbace93f57
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-02-01 11:44:10 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,9 +2,31 @@ const commentArea = document.querySelector(".commentArea");
 const placeholder = commentArea.getAttribute("placeholder");
 
 commentArea.onfocus = function(){
-    this.setAttribute("placeholder", "");
+  this.setAttribute("placeholder", "");
 }
 
 commentArea.onblur = function(){
-    this.setAttribute("placeholder", placeholder);
+  this.setAttribute("placeholder", placeholder);
 }
+
+class User {
+  name;
+  avatar;
+}
+
+class Comment {
+  id;
+  content;
+  score;
+  createdAt;
+
+  increaseScore(){
+    this.score +=1;
+  }
+
+  decreaseScore(){
+    this.score -=1;
+  }
+}
+
+const trial = new User();
